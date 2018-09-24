281b969ec958bf4cfe469b2425e18a8392626ee5
evanpgrimes <36830461+evanpgrimes@users.noreply.github.com>
2018-09-23 20:52:50 -0500

    compare array + csv functionality

--- Assignment0/compareArrays.js
+++ Assignment0/compareArrays.js
@@ -1,10 +1,22 @@
 function compare(A, B) {
-    "use strict"
+    "use strict";
     var correct = 0;
-   // for (i=0; Object.keys(A)-1;i++) {
-    for (var i=1; i<20; i=i+2) {  
-        if (A[i] == B[i]) {
-            correct += 1
+  
+    
+    if (A[1] === ",") {
+        
+        for (var i=2; i<30; i=i+3) {  
+            if (A[i] == B[i]) {
+                correct += 1
+            } 
+        }
+    }
+    else {
+        
+        for (var i=1; i<20; i=i+2) {  
+            if (A[i] == B[i]) {
+                correct += 1
+            }
         }
     }
     
